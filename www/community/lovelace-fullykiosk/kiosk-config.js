9f9c32cf00e25d87d598eaa890ea366b5b06c656
Glenn Morrison <wm.g.morrison@gmail.com>
2019-09-07 19:50:18 -0500

    fully kiosk

---
+++ www/community/lovelace-fullykiosk/kiosk-config.js
@@ -0,0 +1,6 @@
+setTimeout(function() {
+  LovelaceFullyKiosk.bind("3b3eda67-36d67dd5", "light.alarm_panel_screen", "binary_sensor.alarm_panel_motion");
+}, 200);
+setTimeout(function() {
+  LovelaceFullyKiosk.bind("63a59e69-36e6756d", "light.alarm_clock_screen", "binary_sensor.alarm_clock_motion");
+}, 200);
